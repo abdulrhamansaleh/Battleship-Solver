8772dba8ebc8c08d82be13262eeb38fcd50214a1
abdulrhamansaleh <87139691+abdulrhamansaleh@users.noreply.github.com>
2021-07-22 11:44:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-import time
+from tkinter import *
 import tkinter as tk
-from ai_board import ai_board
+from tkinter.constants import CENTER
 
-ai = ai_board()
-ai.place_ships()
-ai.board_frame.update()
-time.sleep(1)
-attack_counter = 0
 
-while True:
-    ai.attack_guess()
-    time.sleep(0.5)
-    ai.board_frame.update()
-    if ai.check_for_win() == 17:
-        break
-time.sleep(0.5)
-win_alert = tk.Label(ai.board_frame, text=f'BOARD HAS BEEN DEFEATED {ai.attack_counter} ATTACKS')
-win_alert.pack()
-win_alert.place(x=50, y=50, width=550, height=550)
-ai.board_frame.mainloop()
+game_frame = tk.Tk()
+game_frame.title("Battle-Ship Solver")
+game_frame.geometry("550x550")
+
+
+
+ai_option_button = tk.Button(game_frame,text="Solve A Randomly Generated Board")
+ai_option_button.place(relx= 0.5,rely= 0.45,anchor=CENTER)
+user_option_button = tk.Button(game_frame,text="Solve A Board Created By Me")
+user_option_button.place(relx= 0.5,rely= 0.5,anchor=CENTER)
+game_frame.mainloop()
