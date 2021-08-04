0c6e67895311b3375ecc01e369dd3ed9f1eb104b
abdulrhamansaleh <87139691+abdulrhamansaleh@users.noreply.github.com>
2021-08-04 17:11:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter.constants import CENTER, HORIZONTAL
-from ai import ai_board
-import time
-from user_board import user_board
-
 
 # Method to load a randomly generated board and solve it with AI
 def start_ai_based():
@@ -59,6 +52,6 @@ ai_option_button = tk.Button(game_frame, text="Solve A Randomly Generated Board"
 ai_option_button.place(relx=0.5, rely=0.45, anchor=CENTER)
 
 user_option_button = tk.Button(game_frame, text="Solve A Board Created By Me", command=start_user_based)
-user_option_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+user_option_button.place(relx=0.5, rely=0.55, anchor=CENTER)
 
 game_frame.mainloop()
